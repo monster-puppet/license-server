ac86a3aca9d7dd398264b5c3a61ecbdac4a6ce92
exe.dev user <exedev@license-server.exe.xyz>
2026-01-03 09:46:02 +0000

    Set MAYA_SCRIPT_PATH to only our scripts path to ensure our userSetup.py runs

--- templates/Launch Maya.bat
+++ templates/Launch Maya.bat
@@ -1,13 +1,11 @@
 @echo off
+setlocal
 
 set "version=2024"
 
 set "regKey=HKEY_LOCAL_MACHINE\SOFTWARE\Autodesk\Maya\%version%\Setup\InstallPath"
 set "current_folder=%~dp0"
 set "maya_root_path=%current_folder%Tools\Maya"
-set "MAYA_MODULE_PATH=%current_folder%Tools\Maya"
-set "MAYA_SCRIPT_PATH=%current_folder%Tools\Maya\scripts;%MAYA_SCRIPT_PATH%"
-set "MAYA_PROJECT=R:\"
 
 :: Check if the Maya Registry Key exists
 reg query "%regKey%" /v MAYA_INSTALL_LOCATION > nul 2>&1
@@ -32,17 +30,22 @@ if not exist "%mayaPath%\bin\maya.exe" (
 
 :: install external python packages
 "%mayaPath%\bin\mayapy.exe" -m pip install --upgrade pip
-"%mayaPath%\bin\mayapy.exe" -m pip install -r "R:\Tools\Maya\requirements.txt"
+"%mayaPath%\bin\mayapy.exe" -m pip install -r "%maya_root_path%\requirements.txt"
 
-:: Find all .mod files and add their directories
-for /r "%MAYA_MODULE_PATH%" %%i in (*.mod) do (
+:: Find all .mod files
+for /r "%maya_root_path%" %%i in (*.mod) do (
     echo Module detected in: %%~dpi
 )
 
 echo.
-echo MAYA_MODULE_PATH = %MAYA_MODULE_PATH%
+echo Launching Maya %version%...
 echo.
 
-:: Launch Maya directly (not with start) so it inherits environment
-cd /d "%mayaPath%\bin"
-"%mayaPath%\bin\maya.exe"
+:: End local and set environment for Maya
+endlocal & (
+    set "MAYA_MODULE_PATH=%current_folder%Tools\Maya"
+    set "MAYA_SCRIPT_PATH=%current_folder%Tools\Maya\scripts"
+    set "MAYA_PROJECT=R:\"
+    cd /d "%mayaPath%\bin"
+    "%mayaPath%\bin\maya.exe"
+)
